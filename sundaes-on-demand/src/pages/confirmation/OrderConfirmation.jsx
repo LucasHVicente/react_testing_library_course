49b407218edb542074ce8b696759941ae43fee73
LucasHVicente <l.vicente9003@gmail.com>
2022-12-27 17:56:33 -0300

    add: test and code error message on OrderConfirmation component

--- sundaes-on-demand/src/pages/confirmation/OrderConfirmation.jsx
+++ sundaes-on-demand/src/pages/confirmation/OrderConfirmation.jsx
@@ -2,15 +2,18 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Button, Container } from "react-bootstrap";
 import { useOrderDetails } from "../../contexts/OrderDetails";
+import AlertBanner from "../common/AlertBanner";
 
 export function OrderConfirmation({ setOrderPhase }) {
   const { resetOrder } = useOrderDetails();
+  const [error, setError] = useState(false);
   const [orderNumber, setOrderNumber] = useState(null);
 
   useEffect(() => {
     axios
       .post("http://localhost:3030/order")
-      .then((response) => setOrderNumber(response.data.orderNumber));
+      .then((response) => setOrderNumber(response.data.orderNumber))
+      .catch((e) => setError(true));
     //TODO extra practice error
   }, []);
 
@@ -20,6 +23,19 @@ export function OrderConfirmation({ setOrderPhase }) {
     setOrderPhase("entry");
   }
 
+  const createNewOrderButton = (
+    <Button onClick={handleCreateNewOrder}>Create new order</Button>
+  );
+
+  if (error) {
+    return (
+      <>
+        <AlertBanner />
+        {createNewOrderButton}
+      </>
+    );
+  }
+
   return (
     <Container>
       {orderNumber ? (
@@ -27,7 +43,7 @@ export function OrderConfirmation({ setOrderPhase }) {
           <h1>Thank you!</h1>
           <h2>Your order number is {orderNumber}</h2>
           <span>as per our terms and conditions, nothing wil happen now</span>
-          <Button onClick={handleCreateNewOrder}>Create new order</Button>
+          {createNewOrderButton}
         </>
       ) : (
         <span>Loading</span>
